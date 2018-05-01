2d5ffa79ac31820bcb8485921816fd2817427422
Federico Miras <miras.federico@gmail.com>
2018-05-01 19:19:35 -0300

    Add XO with prettier and precommit hook for linting

--- test/index.js
+++ test/index.js
@@ -2,7 +2,9 @@ const test = require('ava')
 const rewire = require('rewire')
 
 const validate = rewire('../lib')
-validate.__set__('createError', (code, message) => { throw new Error(message) })
+validate.__set__('createError', (code, message) => {
+  throw new Error(message)
+})
 
 test('validate 2 normal string parameters', t => {
   const parameters = { param1: 'value1', param2: 'value2' }
