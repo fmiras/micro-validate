53ad91e1a17b4cf4dfa16ae2f8cc5df306180074
Federico Miras <miras.federico@gmail.com>
2018-05-01 22:03:35 -0300

    Support custom validator with tests

--- test/index.js
+++ test/index.js
@@ -11,3 +11,28 @@ test('validate 2 normal string parameters', t => {
   const thrower = () => validate(parameters)
   t.notThrows(thrower)
 })
+
+test('validate an empty parameter', t => {
+  const param1 = undefined
+  const parameters = { param1, param2: 'value2' }
+  const thrower = () => validate(parameters)
+  t.throws(thrower)
+})
+
+test('custom validator pass', t => {
+  const parameters = { ssn: 12345678 }
+  const validator = p => {
+    return Number.isInteger(p)
+  }
+  const thrower = () => validate(parameters, validator)
+  t.notThrows(thrower)
+})
+
+test('custom validator fail', t => {
+  const parameters = { ssn: '12345678' }
+  const validator = p => {
+    return Number.isInteger(p)
+  }
+  const thrower = () => validate(parameters, validator)
+  t.throws(thrower)
+})
